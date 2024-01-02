e793a592f42cc58b0a5f0c4913a5f57ed5743aa8
Pranshu Srivastava <pranshusrivastava000@gmail.com>
2024-01-02 22:27:01 +0530

    Refactor 1 done

--- src/main/java/com/example/MoodAnalyser.java
+++ src/main/java/com/example/MoodAnalyser.java
@@ -3,11 +3,23 @@ package com.example;
 import java.util.Arrays;
 
 public class MoodAnalyser {
-    public String analyseMood(String msg) {
-        String words[] = msg.split(" ");
-        boolean check = Arrays.asList(words).contains("Sad");
+
+    String msg = "";
+
+    public MoodAnalyser() {
+    }
+
+    public MoodAnalyser(String msg) {
+        this.msg = msg;
+    }
+
+    public String analyseMood() {
+        this.msg = this.msg.toLowerCase();
+        String words[] = this.msg.split(" ");
+        boolean check = Arrays.asList(words).contains("sad");
         if (check)
             return "SAD";
         return "HAPPY";
     }
+
 }
--- src/test/java/com/example/AppTest.java
+++ src/test/java/com/example/AppTest.java
@@ -1,20 +1,22 @@
 package com.example;
 
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
 
-/**
- * Unit test for simple App.
- */
-public class AppTest 
-{
-    /**
-     * Rigorous Test :-)
-     */
+public class AppTest {
+
+    MoodAnalyser moodAnalyser = null;
+
     @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
+    public void sadTest() {
+        moodAnalyser = new MoodAnalyser("I am in sad Mood");
+        assertEquals("SAD", moodAnalyser.analyseMood());
+    }
+
+    @Test
+    public void happyTest() {
+        moodAnalyser = new MoodAnalyser("I am in Happy Mood");
+        assertEquals("HAPPY", moodAnalyser.analyseMood());
     }
 }
