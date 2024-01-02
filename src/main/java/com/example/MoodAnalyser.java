58e036894abc23f569c385b90af0b6237f7f3272
Pranshu Srivastava <pranshusrivastava000@gmail.com>
2024-01-03 00:11:49 +0530

    Custom exception handling

--- src/main/java/com/example/MoodAnalyser.java
+++ src/main/java/com/example/MoodAnalyser.java
@@ -13,18 +13,37 @@ public class MoodAnalyser {
         this.msg = msg;
     }
 
-    public String analyseMood() {
-        try {
-            this.msg = this.msg.toLowerCase();
-            String words[] = this.msg.split(" ");
-            boolean check = Arrays.asList(words).contains("sad");
-            if (check)
-                return "SAD";
-            return "HAPPY";
-        } catch (NullPointerException e) {
-            return "HAPPY";
+    public String analyseMood() throws MoodAnalysisException {
+        if (this.msg == null) {
+            throw new MoodAnalysisException(MoodAnalysisExceptionType.NullStringException, "String is Null");
         }
+        if (this.msg.isEmpty()) {
+            throw new MoodAnalysisException(MoodAnalysisExceptionType.EmptyStringException, "String is Empty");
+        }
+        this.msg = this.msg.toLowerCase();
+        String words[] = this.msg.split(" ");
+        boolean check = Arrays.asList(words).contains("sad");
+        if (check)
+            return "SAD";
+        return "HAPPY";
 
     }
+}
 
+enum MoodAnalysisExceptionType {
+    NullStringException,
+    EmptyStringException
+}
+
+class MoodAnalysisException extends Exception {
+    public MoodAnalysisExceptionType exceptionType;
+
+    public MoodAnalysisException(MoodAnalysisExceptionType exceptionType, String message) {
+        super(message);
+        this.exceptionType = exceptionType;
+    }
+
+    public MoodAnalysisExceptionType getExceptionType() {
+        return exceptionType;
+    }
 }
--- src/test/java/com/example/AppTest.java
+++ src/test/java/com/example/AppTest.java
@@ -1,6 +1,7 @@
 package com.example;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
 
 import org.junit.Test;
 
@@ -11,19 +12,50 @@ public class AppTest {
     @Test
     public void sadTest() {
         moodAnalyser = new MoodAnalyser("I am in sad Mood");
-        assertEquals("SAD", moodAnalyser.analyseMood());
+        try {
+            String mood = moodAnalyser.analyseMood();
+            assertEquals("SAD", mood);
+        } catch (MoodAnalysisException e) {
+            fail(e.getMessage());
+        }
     }
 
     @Test
     public void happyTest() {
         moodAnalyser = new MoodAnalyser("I am in Happy Mood");
-        assertEquals("HAPPY", moodAnalyser.analyseMood());
+        try {
+            String mood = moodAnalyser.analyseMood();
+            assertEquals("HAPPY", mood);
+        } catch (MoodAnalysisException e) {
+            fail(e.getMessage());
+        }
     }
 
     @Test
     public void checkNUllTest() {
         moodAnalyser = new MoodAnalyser(null);
-        assertEquals("HAPPY", moodAnalyser.analyseMood());
+        try {
+            moodAnalyser.analyseMood();
+            fail("Expected a null string");
+        } catch (MoodAnalysisException e) {
+            if (e.getExceptionType() == MoodAnalysisExceptionType.NullStringException)
+                assertEquals("String is Null", e.getMessage());
+            else
+                fail("Expected a null string but got an empty string");
+        }
     }
 
+    @Test
+    public void checkEmptyTest() {
+        moodAnalyser = new MoodAnalyser("");
+        try {
+            moodAnalyser.analyseMood();
+            fail("Expected an empty string");
+        } catch (MoodAnalysisException e) {
+            if (e.getExceptionType() == MoodAnalysisExceptionType.EmptyStringException)
+                assertEquals("String is Empty", e.getMessage());
+            else
+                fail("Expected an empty string but got null");
+        }
+    }
 }
